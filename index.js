ba4a30dc26e0bccdf3b496102ad367460f25c68f
L00vely <peralesrodriguezdavid@gmail.com>
2023-07-04 19:08:20 -0600

    Conf 4

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const dontenv = require('dotenv').config();
 const { dbConnection } = require('./database/config');
 const cors = require('cors');
 const app = express();
-const serverless = require('serverless-http');
-const router = express.Router();
 
 // Base de datos
 dbConnection();
@@ -15,8 +13,8 @@ app.use( express.json() );
 app.use( cors() );
 
 // Rutas
-app.use('/.netlify/functions/api/auth', require('./routes/auth') );
-app.use('/.netlify/functions/api/events', require('./routes/events') );
+app.use('/api/auth', require('./routes/auth') );
+app.use('/api/events', require('./routes/events') );
 
 
 // Directorio público
@@ -26,6 +24,3 @@ app.use( express.static('public') );
 app.listen( process.env.PORT , () => {
     console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
 } )
-
-
-module.exports.handler = serverless(app);
